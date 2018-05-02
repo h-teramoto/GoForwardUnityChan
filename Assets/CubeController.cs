d5161922a93e80bf4ae2cb470657c3c7f9466fe7
h-teramoto <h-teramoto@naroc.co.jp>
2018-05-03 08:37:10 +0900

    GoForwardUnityChanプロジェクト　初コミット
    課題はCubeController内で実装しています

---
+++ Assets/CubeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+    // キューブの移動速度
+    private float speed = -0.2f;
+
+    // 消滅位置
+    private float deadLine = -10;
+
+    //効果音
+    private AudioSource sound;
+
+    // Use this for initialization
+    void Start()
+    {
+        sound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // キューブを移動させる
+        transform.Translate(this.speed, 0, 0);
+
+        // 画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Block" || col.gameObject.tag == "Ground" )
+        {
+            sound.PlayOneShot(sound.clip);
+            Debug.Log("衝突 OnCollisionEnter2D");
+        }
+        
+        
+    }
+}
